c7c997ea579b817d325d0ddef934a6089509bf59
Suton <sutonommp@gmail.com>
2026-08-23 18:44:27 +0800

    Update production build assets

--- public/build/assets/TestFile-D9s_MHZi.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-DGX9Xzxp.js";function r(){return t.jsx("div",{children:"Test"})}export{r as default};
---
+++ public/build/assets/TestFile-gbca2xnU.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-D8O9hBTh.js";function r(){return t.jsx("div",{children:"Test"})}export{r as default};
