423d08701cec1f5cfbd549ae0f269fb3b3afe464
Suton <sutonommp@gmail.com>
2026-08-25 09:13:11 +0800

    Build: Compile frontend assets for production (Hostinger)

---
+++ public/build/assets/TestFile-BAXARZ_o.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-C881via5.js";function r(){return t.jsx("div",{children:"Test"})}export{r as default};
--- public/build/assets/TestFile-gbca2xnU.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-D8O9hBTh.js";function r(){return t.jsx("div",{children:"Test"})}export{r as default};
